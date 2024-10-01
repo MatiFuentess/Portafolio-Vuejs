ca20e1dc235a352777b5066196c165e03fff459c
Matias Fuentes <matyfuentes9@gmail.com>
2024-09-30 22:08:22 -0300

    Capitulo 3 Barra de navegacion

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,21 @@
 <script setup>
-
-//Puro JS
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Navbar from './components/icons/Navbar.vue';
 </script>
 
 <template>
-  <!--HTML-->
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <header>
+        <!--Barra de navegacion-->
+        <Navbar/>
+    </header>
+    <main>
+        <section id="educacion"><h2>Educación - Cursos</h2></section>
+        <section id="experiencia"><h2>Experiencia</h2></section>
+        <section id="proyectos"><h2>Proyectos</h2></section>
+        <section id="habilidades"><h2>Habilidades</h2></section>
+        <section id="intereses"><h2>Intereses</h2></section>
+    </main>
 </template>
-
+ 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
